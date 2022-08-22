6d69812e81ee20dfa8a50a1c174ff2662ccb7eaa
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 13:59:21 -0300

    Removes eslint-disable from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export { default as Home } from './Home';
 export { default as InitialPage } from './InitialPage';
 export { default as NotFound } from './NotFound';
