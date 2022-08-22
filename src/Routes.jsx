231f3bc73b41b8f1d428b51afdb9c392276064fb
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 11:12:34 -0300

    Implements NotFound page logic in Routes component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import InitialPage from './pages/InitialPage';
+import NotFound from './pages/NotFound';
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
         <Route path="/" element={<InitialPage />}>
           <Route path="tokens" element={<Home />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
