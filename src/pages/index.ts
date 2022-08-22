c65784ac5fbc236e47ca76227d56b6d950817120
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 12:12:36 -0300

    Exports InitialPage and NotFound from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as Home } from './Home';
+export { default as InitialPage } from './InitialPage';
+export { default as NotFound } from './NotFound';
