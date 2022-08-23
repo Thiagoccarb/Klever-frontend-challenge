05ad5da0e3bf0af02dea0dc83ab8eadaca889608
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-23 16:37:52 -0300

    Exports CustomCurrencyInput from index

--- src/components/genericComponents/index.ts
+++ src/components/genericComponents/index.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable import/no-cycle */
 export { default as WishWallet } from './WishWallet';
 export { default as CustomButton } from './CustomButton';
 export { default as CustomInput } from './CustomInput';
+export { default as CustomCurrencyInput } from './CustomCurrencyInput';
