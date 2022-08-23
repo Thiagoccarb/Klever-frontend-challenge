8c0fd74e12c323cb0304d70d7e63bf62f22a6bed
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 23:51:44 -0300

    Implements Home components logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,28 @@
+// import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import useStyles from '../styles/styles';
+
+import {
+  WishWallet,
+  CustomButton,
+} from '../components/genericComponents';
 
 function Home() {
+  const { primaryButton } = useStyles();
   return (
-    <Typography variant="h1">
-      Heading
-    </Typography>
+    <Grid
+      container
+      component="section"
+      id="wallet-container"
+    >
+      <WishWallet />
+      <CustomButton
+        className={primaryButton}
+      >
+        Add Token
+      </CustomButton>
+    </Grid>
   );
 }
 
