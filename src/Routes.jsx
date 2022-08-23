6cfe51a1133d364044bd31f0f6e4225a5c045725
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-23 12:16:20 -0300

    Implements EditToken route logic

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import {
-  Home, InitialPage, NotFound, AddToken,
+  Home, InitialPage, NotFound, AddToken, EditToken,
 } from './pages';
 
 function AppRoutes() {
@@ -12,7 +12,7 @@ function AppRoutes() {
         <Route path="/" element={<InitialPage />}>
           <Route path="tokens" element={<Home />} />
           <Route path="tokens/add" element={<AddToken />} />
-
+          <Route path="tokens/edit/:id" element={<EditToken />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
