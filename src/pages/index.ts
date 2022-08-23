9a444d539c67d2d96bd7f3f0418e9a5bce8e4e59
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-23 12:16:03 -0300

    Exports EditToken from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from './Home';
 export { default as InitialPage } from './InitialPage';
 export { default as NotFound } from './NotFound';
 export { default as AddToken } from './AddToken';
+export { default as EditToken } from './EditToken';
