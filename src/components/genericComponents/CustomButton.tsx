9b956e173e40d90d59799971b8b71b2a156b16c9
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-23 15:30:58 -0300

    Refactors CustomButton

--- src/components/genericComponents/CustomButton.tsx
+++ src/components/genericComponents/CustomButton.tsx
@@ -2,11 +2,16 @@
 import React, { PropsWithChildren } from 'react';
 import ButtonUnstyled from '@mui/base/ButtonUnstyled';
 
-function CustomButton(props: PropsWithChildren) {
+function CustomButton(props: PropsWithChildren<{
+  children: string;
+  className: string;
+  onClick: () => void;
+}>) {
   const { children, ...buttonProps } = props;
   return (
     <ButtonUnstyled
       {...buttonProps}
+      type="submit"
     >
       {children}
     </ButtonUnstyled>
