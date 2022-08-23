effbe98512c03231bb313b892c09b8aedcdc0314
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 23:51:18 -0300

    Implements Grid component logic

--- src/pages/InitialPage.jsx
+++ src/pages/InitialPage.jsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
+import Grid from '@material-ui/core/Grid';
+// import Typography from '@material-ui/core/Typography';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
+import logo from '../assets/logo.svg';
+
+import useStyles from '../styles/styles';
+
 function InitialPage() {
+  const { header, main } = useStyles();
   const { pathname } = useLocation();
   const isHome = pathname === '/tokens';
   return (
-    <>
+    <Grid
+      component="main"
+      className={main}
+    >
       {
         !isHome
         && <Navigate to="/tokens" />
       }
+      <Grid
+        className={header}
+        component="header"
+      >
+        <img src={logo} alt="Klever-logo" id="logo" />
+      </Grid>
       <Outlet />
-    </>
+    </Grid>
   );
 }
 
