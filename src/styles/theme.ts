477cc3272a4bc4ba349d137cd473d23a68b8e104
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-22 23:52:12 -0300

    Creates global theme component

---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+import { createTheme } from '@material-ui/core';
+
+const paletteTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#AA33B5',
+    },
+    secondary: {
+      main: '#13152A',
+    },
+    warning: {
+      main: '#920000',
+    },
+  },
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Shoika Regular',
+      'sans-serif',
+    ].join(','),
+    h1: {
+      fontSize: '5vmin',
+      color: '#fff',
+      margin: 0,
+      padding: 0,
+      '@media (max-width:400px)': {
+        fontSize: '1rem',
+      },
+    },
+  },
+  palette: {
+    primary: {
+      main: paletteTheme.palette.primary.main,
+    },
+    secondary: {
+      main: paletteTheme.palette.secondary.main,
+    },
+    warning: {
+      main: paletteTheme.palette.warning.main,
+    },
+  },
+});
+
+export default theme;
