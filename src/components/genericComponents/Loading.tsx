2f1d5af49c63757a8d1accb15d825ca1534cd42d
Thiago Cabral Carboneri <thiago.carboneri@ringa.com.br>
2022-08-24 09:28:08 -0300

    Creates Loading component

---
+++ src/components/genericComponents/Loading.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+interface IProps {
+  open: boolean;
+}
+export default function Loading({ open }: IProps) {
+  return (
+    <div>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+  );
+}
